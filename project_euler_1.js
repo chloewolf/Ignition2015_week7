16172ed4f0ee1eaf5b3de50b887b857fb78a46ff
Chloe Wolf <chwo0667@colorado.edu>
2015-04-02 18:55:53 -0600

    Update project_euler_1.js

--- project_euler_1.js
+++ project_euler_1.js
@@ -1,1 +1,16 @@
+var sum = 0;
 
+for (var num = 0; num < 1000; num++) {
+   
+   
+    if(num % 3 === 0 || num % 5 === 0 )  
+    {
+       sum = (num + sum);  
+    }
+    
+}
+
+ console.log(sum);
+ 
+ 
+ answer: 233168
